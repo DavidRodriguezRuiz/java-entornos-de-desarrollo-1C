d431610dc09cebc94ab30a1c31b63cc5ce50eab4
DavidRodriguezRuiz <davidrodriguezruiz7441@gmail.com>
2024-04-23 17:16:51 +0200

    EdadPersonas actualizado con comentarios

--- EdadPersonas/src/test/TestEdadPersonas.java
+++ EdadPersonas/src/test/TestEdadPersonas.java
@@ -7,6 +7,10 @@ import models.Persona;
 
 class TestEdadPersonas {
 
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 0
+	 */
 	@Test
 	void testPersona0() {
 		Persona p = new Persona ("nombre", "apellidos", 0);
@@ -15,6 +19,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 17
+	 */
 	@Test
 	void testPersona17() {
 		Persona p = new Persona ("nombre", "apellidos", 17);
@@ -23,6 +31,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 18
+	 */
 	@Test
 	void testPersona18() {
 		Persona p = new Persona ("nombre", "apellidos", 18);
@@ -31,6 +43,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 65
+	 */
 	@Test
 	void testPersona65() {
 		Persona p = new Persona ("nombre", "apellidos", 65);
@@ -39,6 +55,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 66
+	 */
 	@Test
 	void testPersona66() {
 		Persona p = new Persona ("nombre", "apellidos", 66);
@@ -47,6 +67,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un mensaje si la variable edad si está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 130
+	 */
 	@Test
 	void testPersona130() {
 		Persona p = new Persona ("nombre", "apellidos", 130);
@@ -55,6 +79,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un error si la variable edad no está en el rango indicado. (De 0 a 130)
+	 * Variable edad = -1
+	 */
 	@Test
 	void testPersonaEdadInvalidaMenos1() {
 		Exception excepcion = assertThrows(IllegalArgumentException.class, 
@@ -64,6 +92,10 @@ class TestEdadPersonas {
 		assertEquals(esperado, obtenido);
 	}
 	
+	/**
+	 * Devuelve un error si la variable edad no está en el rango indicado. (De 0 a 130)
+	 * Variable edad = 131
+	 */
 	@Test
 	void testPersonaEdadInvalida131() {
 		Exception excepcion = assertThrows(IllegalArgumentException.class, 
